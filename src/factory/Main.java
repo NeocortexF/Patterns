aeeaad35c41be74499582dc1bbea40bbed389c88
NeocortexF <eugeneshamkin@mail.ru>
2017-03-22 18:32:30 +0500

    Factory Pattern example

---
+++ src/factory/Main.java
@@ -0,0 +1,20 @@
+package factory;
+
+public class Main {
+    public static void main(String[] args) {
+       DeveloperFactory developerFactory = createDeveloperBySpecialty("java");
+       Developer developer = developerFactory.createDeveloper();
+
+       developer.writeCode();
+    }
+
+    static DeveloperFactory createDeveloperBySpecialty(String specialty) {
+        if (specialty.equalsIgnoreCase("java")) {
+            return new JavaDeveloperFactory();
+        } else if (specialty.equalsIgnoreCase("Cpp")) {
+            return new CppDeveloperFactory();
+        } else {
+            throw new RuntimeException(specialty + " is unknown!");
+        }
+    }
+}
